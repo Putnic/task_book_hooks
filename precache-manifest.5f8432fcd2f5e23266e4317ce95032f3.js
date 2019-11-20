89100f259c08f723fe4169dd56dfd0c1c47fbd73
Dmitry Polyakov <inform80@mail.ru>
2019-11-20 13:19:05 +0500

    Updates

---
+++ precache-manifest.5f8432fcd2f5e23266e4317ce95032f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7320d4cf08c8a1876340eaaacf43416b",
+    "url": "/task_book_hooks/index.html"
+  },
+  {
+    "revision": "6344cf038126fde0100f",
+    "url": "/task_book_hooks/static/css/main.6b2006c7.chunk.css"
+  },
+  {
+    "revision": "9452e2e53f08aa292da5",
+    "url": "/task_book_hooks/static/js/2.6423c7b8.chunk.js"
+  },
+  {
+    "revision": "6344cf038126fde0100f",
+    "url": "/task_book_hooks/static/js/main.f9d84015.chunk.js"
+  },
+  {
+    "revision": "d88e897bfca793ab7192",
+    "url": "/task_book_hooks/static/js/runtime-main.08c0fd14.js"
+  }
+]);
